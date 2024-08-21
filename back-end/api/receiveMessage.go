1ea53369f5028fb850a3fe094b307549cc1270cb
Eiichi Sayama <eiichimouse@outlook.jp>
2024-08-21 09:46:06 +0900

    fix:Add log point

--- back-end/api/receiveMessage.go
+++ back-end/api/receiveMessage.go
@@ -3,7 +3,7 @@ package api
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -23,6 +23,7 @@ func ReceiveMessage(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	redisUrl := os.Getenv("REDIS_URL")
+	log.Println("redisUrl : ", redisUrl)
 
 	opt, err := redis.ParseURL(redisUrl)
 	if err != nil {
@@ -30,30 +31,31 @@ func ReceiveMessage(w http.ResponseWriter, r *http.Request) {
 	}
 	client := redis.NewClient(opt)
 	ctx := context.Background()
+	log.Println("Connection established")
 
 	//get last 10 messages in one room
 
 	result := client.Set(ctx, "foo", "sampleMessage", 0).Err()
-	fmt.Println("Set : ", result)
+	log.Println("Set : ", result)
 
 	val2, err := client.Get(ctx, "foo").Result()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Simple string")
-	fmt.Println("foo", val2)
+	log.Println("Simple string")
+	log.Println("foo", val2)
 
 	val3 := client.ZAdd(ctx, "zadd-key", redis.Z{Member: "member1", Score: 1})
 	val4 := client.ZAdd(ctx, "zadd-key", redis.Z{Member: "member2", Score: 2})
 	val5 := client.ZAdd(ctx, "zadd-key", redis.Z{Member: "member3", Score: 3})
-	fmt.Println(val3, val4, val5)
+	log.Println(val3, val4, val5)
 
 	val6, err := client.ZRangeByScore(ctx, "zadd-key", &redis.ZRangeBy{Min: "-inf", Max: "+inf", Offset: 0, Count: 5}).Result()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Use ZrangeByScore")
-	fmt.Println(val6)
+	log.Println("Use ZrangeByScore")
+	log.Println(val6)
 
 	// val, err := client.ZRangeByScore(ctx, roomId, &redis.ZRangeBy{Offset: 10, Count: 5}).Result()
 	// if err != nil {
