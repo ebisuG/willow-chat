b893d67e1d34352aef2ad04901bb684fabefeed8
Eiichi Sayama <eiichimouse@outlook.jp>
2024-08-22 16:54:16 +0900

    feat:Add login/logout pages, buttons

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,41 @@
 import './App.css'
 import Chat from './Chat'
+import { Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+// import { HomePage } from "./pages/Home";
+import { Secret } from "./pages/Secret";
+import { useRoutes } from "react-router-dom"
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AuthProvider } from "./hooks/useAuth";
 
 function App() {
-  return (
-    <>
-        <Chat></Chat>
+  // const routes = useRoutes([
+  //   { path: "/", element: <LoginPage></LoginPage> },
+  //   { path: "/login", element: <LoginPage></LoginPage> },
+  //   { path: "/chat", element: <Chat></Chat> },
+  //   {
+  //     path: "/secret", element:
+  //       <ProtectedRoute >
+  //         <Secret />
+  //       </ProtectedRoute >
+  //   }
+  // ])
 
-    </>
-  )
+
+  return (<AuthProvider>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      {/* <Route path="/login" element={<LoginPage />} /> */}
+      <Route
+        path="/secret"
+        element={
+          <ProtectedRoute>
+            <Secret />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  </AuthProvider>)
 }
 
 export default App
---
+++ front-end/src/components/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+export const ProtectedRoute: React.FC<{ children: React.ReactNode }>
+    = ({ children }) => {
+        const isAuth = useAuth()?.user?.isAuth;
+        if (!isAuth) {
+            // user is not authenticated
+            return <Navigate to="/login" />;
+        }
+        return children;
+    };
