f7d057e78aba63419644bbab6e7fc67d191c3c9d
Eiichi Sayama <eiichimouse@outlook.jp>
2024-08-23 01:47:36 +0900

    fix:Modify default value to true to show chat page

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
       <AuthProvider>
         <ProtectedRoute>
           <Secret />
-        </ProtectedRoute>,
+        </ProtectedRoute>
       </AuthProvider>
   },
 ]);
